b0643ad9aa69b0f08938f79532d187743c785409
Indahs11 <indahsrw11@gmail.com>
2021-02-23 20:26:45 +0700

    Frontend-Customer-toko-komputer

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+const base_url = "http://localhost:7880/store/api/v1"
+const product_image_url = "http://localhost:7880/product_image/"
+const customer_image_url = "http://localhost:7880/image/"
+export {
+    base_url,
+    product_image_url,
+    customer_image_url
+}
